b4101e8e4e80d40129c45f7fa750f867f2ac9916
OMouta <tiagomouta609@gmail.com>
2026-01-24 20:07:17 +0000

    remove example

--- example/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-// Boot RapoPHP with App namespace
-$app = Rapo\Bootstrap::boot('App', __DIR__ . '/src');
-
-// Handle the app
-$app->handle();
